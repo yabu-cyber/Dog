7ea5b70b699b0e7bc98683a97e8f6ee40ff9d8ef
yabu-cyber <meiwakumellikotowari@gmail.com>
2020-06-20 23:09:05 +0900

    compleate

--- JS/JSDog.js
+++ JS/JSDog.js
@@ -1,6 +1,23 @@
 
-$(function() {
-  // 以下で、hideメソッドを用いて<h1>要素を隠してください
-  $('h1').hide();
 
-});
+$(function() {
+        $('button').click(
+          function() {
+            $.ajax("https://dog.ceo/api/breeds/image/random" ,
+              {
+                dataType:'json',
+                type:'get'
+              }
+            )
+            .done(function(data){
+              console.log(data);
+              //$("body").append("<img src='"+data.message+"'>");
+              $(".dog-image").attr({
+                src:data.message
+              })
+            }
+
+          )}
+
+        );
+      });
